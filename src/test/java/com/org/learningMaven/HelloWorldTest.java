227841dc2c26384876d0e1f53a4b587065779006
ZaiweiXiong <Zaiwei.xiong@gmail.com>
2016-05-24 21:35:12 +0800

    updeted class
    updeted class

--- src/test/java/com/org/learningMaven/HelloWorldTest.java
+++ src/test/java/com/org/learningMaven/HelloWorldTest.java
@@ -1,11 +1,16 @@
 package com.org.learningMaven;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class HelloWorldTest {
 
 	@Test
 	public void Login(){
+		
+		WebDriver driver = new FirefoxDriver ();
+		
 		System.out.println("Login testing maven testng and");
-	}
+	}   
 }
