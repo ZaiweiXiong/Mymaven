9a3b584d729d5ba726cc8f723c481336d940a884
Xiong, Zaiwei <Zaiwei.Xiong@ca.com>
2016-05-25 14:49:38 +0800

    updated files
    updated files

--- src/test/java/com/org/learningMaven/HelloWorldTest.java
+++ src/test/java/com/org/learningMaven/HelloWorldTest.java
@@ -14,9 +14,16 @@ public class HelloWorldTest {
 		
 		//WebDriver driver = new FirefoxDriver ();
 		
-		//WebDriver driver = DesiredCapabilitiesBrowser.webBroswer("http://155.35.154.157:4444/wd/hub", "en", 1200,1200);
-		//driver.get("http://www.google.com");
+		WebDriver driver = DesiredCapabilitiesBrowser.webBroswer("http://155.35.154.157:4444/wd/hub", "en", 1200,1200);
+		driver.get("http://www.google.com");
+		try {
+			Thread.sleep(3000);
+		}catch (Exception error) {
+			error.printStackTrace();
+		}
 		
+		
+		driver.close();
 		System.out.println("Login testing maven testng and selenium webdriver");
 	} 
 	@Test
