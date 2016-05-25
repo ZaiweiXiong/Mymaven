aedf97463f15665873d3b86712b61621fa77df0e
Xiong, Zaiwei <Zaiwei.Xiong@ca.com>
2016-05-25 13:23:08 +0800

    updated
    updated

--- src/test/java/com/org/learningMaven/HelloWorldTest.java
+++ src/test/java/com/org/learningMaven/HelloWorldTest.java
@@ -1,17 +1,21 @@
 package com.org.learningMaven;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
+
+import com.DesiredCapabilitiesBrowser;
+
+
 
 public class HelloWorldTest {
 
 	@Test
 	public void Login(){
 		
-		WebDriver driver = new FirefoxDriver ();
+		//WebDriver driver = new FirefoxDriver ();
 		
-		driver.get("http://www.google.com");
+		//WebDriver driver = DesiredCapabilitiesBrowser.webBroswer("http://155.35.154.157:4444/wd/hub", "en", 1200,1200);
+		//driver.get("http://www.google.com");
 		
 		System.out.println("Login testing maven testng and selenium webdriver");
 	} 
